7144611762679c6b8feee03e64e4235b8eceec72
Nelson <nelsonhenrique11@gmail.com>
2021-06-23 19:58:35 -0300

    Aula 6 e 7 completas

--- src/java/br/recife/edu/ifpe/controller/servlets/ProdutoServlet.java
+++ src/java/br/recife/edu/ifpe/controller/servlets/ProdutoServlet.java
@@ -6,6 +6,7 @@ import br.recife.edu.ifpe.model.repositorios.RepositorioEstoque;
 import br.recife.edu.ifpe.model.repositorios.RepositorioProdutos;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,46 +32,82 @@ public class ProdutoServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
-    int codigo = 0;
-    try {
-      codigo = Integer.parseInt(request.getParameter("codigo"));
-    } catch (NumberFormatException e) {
+    String codigoAux = request.getParameter("codigo");
+    if (codigoAux == null) {
+      List<Produto> produtos = RepositorioProdutos.getCurrentInstance().readAll();
       response.setContentType("text/html;charset=UTF-8");
       try (PrintWriter out = response.getWriter()) {
         out.println("<!DOCTYPE html>");
-        out.println("<html>");
+        out.println("<html lang=\"pt-br\">");
         out.println("<head>");
-        out.println("<title>Servlet ProdutoServlet</title>");    
+        out.println("<title>Servlet ProdutoServlet</title>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<style>th, td {border: 1px solid #666;} caption {font-size: 1.5em; font-weight: 800;}</style>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1 style=\"color:#f00;\">Produto não encontrado</h1>");
+        out.println("<table>");
+        out.println("<caption>Produtos Cadastrados</caption>");
+        out.println("<tr><th>Código</th><th>Nome</th><th>Marca</th><th>Categoria</th><th>Operações</th></tr>");
+        for (Produto p: produtos) {
+          out.println("<tr>");
+          out.println("<td>" + p.getCodigo() + "</td>");
+          out.println("<td>" + p.getNome() + "</td>");
+          out.println("<td>" + p.getMarca() + "</td>");
+          out.println("<td>" + p.getCategoria() + "</td>");
+          out.println("<td><a href=\"ProdutoServlet?codigo=" + p.getCodigo() + "\">Visualizar</a></td>");
+        }
+        out.println("</table>");
         out.println("<a href=\"index.html\">Página Inicial</a>");
         out.println("</body>");
         out.println("</html>");
       }
-    }
+    } else {
+      int codigo = 0;
+      try {
+        codigo = Integer.parseInt(codigoAux);
+      } catch (NumberFormatException e) {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+          out.println("<!DOCTYPE html>");
+          out.println("<html lang=\"pt-br\">");
+          out.println("<head>");
+          out.println("<title>Servlet ProdutoServlet</title>");
+          out.println("<meta charset=\"utf-8\" />");
+          out.println("</head>");
+          out.println("<body>");
+          out.println("<h1 style=\"color:#f00;\">Produto não encontrado</h1>");
+          out.println("<ul>");
+          out.println("<li><a href=\"index.html\">Página inicial</a></li>");
+          out.println("<li><a href=\"cadastroproduto.html\">Cadastrar Produto</a></li>");
+          out.println("</ul>");
+          out.println("</body>");
+          out.println("</html>");
+        }
+      }
     Produto p = RepositorioProdutos.getCurrentInstance().read(codigo);
     
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter()) {
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Servlet ProdutoServlet</title>");
-      out.println("<style>li {list-style: none;}</style>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1 style=\"color:#1d7d36;\">Produto Recuperado</h1>");
-      out.println("<ul>");
-      out.println("<li>Código:    " + p.getCodigo() + "</li>");
-      out.println("<li>Nome:      " + p.getNome() + "</li>");
-      out.println("<li>Marca:     " + p.getMarca() + "</li>");
-      out.println("<li>Categoria: " + p.getCategoria() + "</li>");
-      out.println("<li>Descrição: " + p.getDescricao() + "</li>");
-      out.println("</ul>");
-      out.println("<a href=\"index.html\">Página Inicial</a>");
-      out.println("</body>");
-      out.println("</html>");
+      response.setContentType("text/html;charset=UTF-8");
+      try (PrintWriter out = response.getWriter()) {
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang=\"pt-br\">");
+        out.println("<head>");
+        out.println("<title>Servlet ProdutoServlet</title>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<style>li {list-style: none;}</style>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1 style=\"color:#1d7d36;\">Produto Recuperado</h1>");
+        out.println("<ul>");
+        out.println("<li>Código:    " + p.getCodigo() + "</li>");
+        out.println("<li>Nome:      " + p.getNome() + "</li>");
+        out.println("<li>Marca:     " + p.getMarca() + "</li>");
+        out.println("<li>Categoria: " + p.getCategoria() + "</li>");
+        out.println("<li>Descrição: " + p.getDescricao() + "</li>");
+        out.println("</ul>");
+        out.println("<a href=\"index.html\">Página Inicial</a>");
+        out.println("</body>");
+        out.println("</html>");
+      }
     }
   }
 
@@ -85,25 +122,13 @@ public class ProdutoServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
-    int codigo = 0;
-    try {
-      codigo = Integer.parseInt(request.getParameter("codigo"));
-    } catch(NumberFormatException e) {
-      response.setContentType("text/html;charset=UTF-8");
-      try (PrintWriter out = response.getWriter()) {
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Servlet ProdutoServlet</title>");      
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1 style=\"color:#f00;\">Erro! O código do produto deve ser um valor numérico.</h1>");
-        out.println("<a href=\"index.html\">Página inicial</a>");
-        out.println("<a href=\"cadastroproduto.html\">Cadastrar outro produto</a>");
-        out.println("</body>");
-        out.println("</html>");
-      }
-    }
+    int codigo;
+    
+    /*
+     * Apesar de preferir corrigir este erro apenas trocando o input do tipo text para number no formulário do
+     * 'cadastroproduto.html', escolhi usar o try-catch em uma função recursiva.
+     */
+    codigo = avaliarCodigoTipoNumerico(response, request.getParameter("codigo"));
     String nome = request.getParameter("nome");
     String marca = request.getParameter("marca");
     String categoria = request.getParameter("categoria");
@@ -126,14 +151,17 @@ public class ProdutoServlet extends HttpServlet {
     response.setContentType("text/html;charset=UTF-8");
     try (PrintWriter out = response.getWriter()) {
       out.println("<!DOCTYPE html>");
-      out.println("<html>");
+      out.println("<html lang=\"pt-br\">");
       out.println("<head>");
-      out.println("<title>Servlet ProdutoServlet</title>");      
+      out.println("<title>Servlet ProdutoServlet</title>");
+      out.println("<meta charset=\"utf-8\" />");
       out.println("</head>");
       out.println("<body>");
       out.println("<h1 style=\"color:#1d7d36;\">Produto " + p.getNome() + " adicionado com sucesso.</h1>");
-      out.println("<a href=\"index.html\">Página inicial</a>");
-      out.println("<a href=\"cadastroproduto.html\">Cadastrar outro produto</a>");
+      out.println("<ul>");
+      out.println("<li><a href=\"index.html\">Página inicial</a></li>");
+      out.println("<li><a href=\"cadastroproduto.html\">Cadastrar outro produto</a></li>");
+      out.println("</ul>");
       out.println("</body>");
       out.println("</html>");
     }
@@ -158,5 +186,49 @@ public class ProdutoServlet extends HttpServlet {
   public String getServletInfo() {
     return "Short description";
   }// </editor-fold>
+  
+  int avaliarCodigoTipoNumerico(HttpServletResponse response, String codigoAuxiliar) throws ServletException, IOException {
+    int codigo = 0;
+    boolean isCorreto = true;
+    try {
+      codigo = Integer.parseInt(codigoAuxiliar);
+    } catch(NumberFormatException e) {
+      isCorreto = false;
+      response.setContentType("text/html;charset=UTF-8");
+      try (PrintWriter out = response.getWriter()) {
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang=\"pt-br\">");
+        out.println("<head>");
+        out.println("<title>Servlet ProdutoServlet</title>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
+        out.println("<link rel=\"stylesheet\" href=\"cadastroproduto-estilo.css\" />");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1 style=\"color:#f00;text-align:center;\">Erro! O Cadastro do Produto é Inválido.</h1>");
+        out.println("<p style=\"color:#f00;text-align:center;\">O código do produto deve ter apenas caracteres numéricos.</p>");
+        out.println("<div id=\"container\">");
+        out.println("<form method=\"post\" action=\"ProdutoServlet\">");
+        out.println("<ul>");
+        out.println("<li><label>Código: <input type=\"text\" name=\"codigo\"/></label></li>");
+        out.println("<li><label>Nome: <input type=\"text\" name=\"nome\" class=\"tabulation\"/></label></li>");
+        out.println("<li><label>Marca: <input type=\"text\" name=\"marca\" class=\"tabulation\"/></label></li>");
+        out.println("<li><label>Categoria: <input type=\"text\" name=\"categoria\"/></label></li>");
+        out.println("<li><label>Descrição: <textarea name=\"descricao\" cols=\"22\"></textarea></label></li>");
+        out.println("<li><input type=\"submit\" value=\"cadastrar\"/></li>");
+        out.println("<li><a href=\"index.html\">Página Inicial</a></li>");
+        out.println("</ul>");
+        out.println("</form>");
+        out.println("</div>");
+        out.println("</body>");
+        out.println("</html>");
+      }
+    }
+    if (isCorreto) {
+      return codigo;
+    } else {
+      return avaliarCodigoTipoNumerico(response, codigoAuxiliar);
+    }
+  }
 
 }
