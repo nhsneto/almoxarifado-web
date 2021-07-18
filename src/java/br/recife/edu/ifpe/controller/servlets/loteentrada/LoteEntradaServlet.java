c16fdc88eb9792e7f16b882c0d8a0fbba378f6b8
Nelson <nelsonhenrique11@gmail.com>
2021-07-18 16:05:01 -0300

    Aula 21 completa

---
+++ src/java/br/recife/edu/ifpe/controller/servlets/loteentrada/LoteEntradaServlet.java
@@ -0,0 +1,69 @@
+package br.recife.edu.ifpe.controller.servlets.loteentrada;
+
+import br.recife.edu.ifpe.model.classes.ItemEntrada;
+import br.recife.edu.ifpe.model.classes.LoteEntrada;
+import br.recife.edu.ifpe.model.classes.Produto;
+import br.recife.edu.ifpe.model.repositorios.RepositorioProdutos;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Nelson
+ */
+@WebServlet(name = "LoteEntradaServlet", urlPatterns = {"/LoteEntradaServlet"})
+public class LoteEntradaServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+  }
+
+  @Override
+  protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doPut(req, resp);
+    int codigo = Integer.parseInt(req.getParameter("codigo"));
+    HttpSession session = req.getSession();
+    LoteEntrada loteEntrada = (LoteEntrada) session.getAttribute("loteEntrada");
+    if (loteEntrada == null) {
+      loteEntrada = new LoteEntrada();
+      session.setAttribute("loteEntrada", loteEntrada);
+    }
+    boolean controle = false;
+    for (ItemEntrada item: loteEntrada.getItens()) {
+      if (item.getProduto().getCodigo() == codigo) {
+        item.setQuantidade(item.getQuantidade() + 1);
+        controle = true;
+      }
+    }
+    if (!controle) {
+      ItemEntrada item = new ItemEntrada();
+      Produto p = RepositorioProdutos.getCurrentInstance().read(codigo);
+      item.setProduto(p);
+      item.setCodigo((int) (Math.random() * 10000));
+      item.setQuantidade(1);
+      loteEntrada.addItem(item);
+      session.setAttribute("msg", "O produto " + p.getNome() + " foi inserido no lote com sucesso.");
+    } else {
+      Produto p = RepositorioProdutos.getCurrentInstance().read(codigo);
+      session.setAttribute("msg", "O produto " + p.getNome() + " foi incrementado no lote com sucesso.");
+    }
+  }
+
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }
+
+}
--- web/insercao-lote-entrada.jsp
+++ web/insercao-lote-entrada.jsp
@@ -12,6 +12,7 @@
       tr > td:not(:first-child) {text-align: center;}
       caption {font-size: 1.2em; font-weight: 800;}
       table {margin-bottom: 2em;}
+      .plus {font-size: 20px; font-weight: 900; color: #2dbd53; text-decoration: none;}
     </style>
   </head>
   <body>
@@ -24,6 +25,7 @@
         <th>Nome</th>
         <th>Marca</th>
         <th>Categoria</th>
+        <th>Inserir</th>
       </tr>
       <c:forEach var="p" items="${produtos}">
         <tr>
@@ -31,6 +33,7 @@
           <td>${p.nome}</td>
           <td>${p.marca}</td>
           <td>${p.categoria}</td>
+          <td><a href="#" class="plus" onclick="adiciona(${p.codigo})">+</a></td>
         </tr>
       </c:forEach>
     </table>
@@ -50,6 +53,34 @@
         </tr>
       </c:forEach>
     </table>
+    <table>
+      <caption>Lote Entrada Produtos</caption>
+      <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Marca</th>
+        <th>Categoria</th>
+        <th>Quantidade</th>
+        <th>Inserir</th>
+      </tr>
+      <c:forEach var="i" items="${loteEntrada.itens}">
+        <tr>
+          <td>${i.produto.codigo}</td>
+          <td>${i.produto.nome}</td>
+          <td>${i.produto.marca}</td>
+          <td>${i.produto.categoria}</td>
+          <td>${i.quantidade}</td>
+          <td><a href="#" class="plus" onclick="adiciona(${p.codigo})">+</a></td>
+        </tr>
+      </c:forEach>
+    </table>
     <a href="index.html">Página Inicial</a>
+    <script>
+      function adiciona(codigo) {
+        fetch("LoteEntradaServlet?codigo=" + codigo, {method: "put"}).then(function() {
+          location.reload();
+        });
+      }
+    </script>
   </body>
 </html>
